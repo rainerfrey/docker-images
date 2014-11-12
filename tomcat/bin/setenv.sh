fa299ee4d6077ffe28278c99631853c9bc3b41ad
Rainer Frey <frey.rainer@gmail.com>
2014-11-12 22:37:56 +0100

    Use separate CATALINA_BASE and omit default web applications

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -1,5 +1,5 @@
-jmx_access="-Dcom.sun.management.jmxremote.access.file=${CATALINA_HOME}/conf/jmxremote.access"
-jmx_password="-Dcom.sun.management.jmxremote.password.file=${CATALINA_HOME}/conf/jmxremote.password"
+jmx_access="-Dcom.sun.management.jmxremote.access.file=${CATALINA_BASE}/conf/jmxremote.access"
+jmx_password="-Dcom.sun.management.jmxremote.password.file=${CATALINA_BASE}/conf/jmxremote.password"
 jmx_ssl="-Dcom.sun.management.jmxremote.ssl=false"
 jmx_host="-Djava.rmi.server.hostname=192.168.59.103"
 random="-Djava.security.egd=file:/dev/./urandom"
