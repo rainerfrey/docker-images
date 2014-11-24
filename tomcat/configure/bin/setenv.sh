0b9c6ff0eae6f8728fccc94525fdead99c8605b3
Rainer Frey <frey.rainer@gmail.com>
2014-11-24 21:25:12 +0100

    Use a separate log base to be able to put logs on a volume

--- tomcat/configure/bin/setenv.sh
+++ tomcat/configure/bin/setenv.sh
@@ -3,4 +3,13 @@ jmx_password="-Dcom.sun.management.jmxremote.password.file=${CATALINA_BASE}/conf
 jmx_ssl="-Dcom.sun.management.jmxremote.ssl=false"
 jmx_host="-Djava.rmi.server.hostname=192.168.59.103"
 random="-Djava.security.egd=file:/dev/./urandom"
-CATALINA_OPTS="${jmx_access} ${jmx_password} ${jmx_ssl} ${jmx_host} ${random} ${CATALINA_OPTS}"
+
+echo "Logbase: $LOG_BASE"
+
+if [ ! -z "$LOG_BASE" ]; then
+	mkdir -p "$LOG_BASE"
+else
+	LOG_BASE="${CATALINA_BASE}/logs"
+fi
+CATALINA_OUT="${LOG_BASE}/catalina.out"
+CATALINA_OPTS="${jmx_access} ${jmx_password} ${jmx_ssl} ${jmx_host} ${random} -Dlog.base=${LOG_BASE} ${CATALINA_OPTS}"
