56421486ffc7e3136bf55cd87810b1e7e84ff62b
Rainer Frey <frey.rainer@gmail.com>
2016-09-12 15:33:18 +0200

    commit current state

--- elk/logstash-shipper-tomcat/run.sh
+++ elk/logstash-shipper-tomcat/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ES_HOST=${ES_PORT_9200_TCP_ADDR:-127.0.0.1}
+
 ES_PORT=${ES_PORT_9200_TCP_PORT:-9200}
 
-sed -i -r -e "s/%ES_HOST%/${ES_HOST}/" -e "s/%ES_PORT%/${ES_PORT}/" /etc/logstash/11_output_elasticsearch.conf
+sed -i -r -e "/port =>/ s/\d+/${ES_PORT}/" /etc/logstash/11_output_elasticsearch.conf
 
 exec /opt/logstash/bin/logstash agent -f /etc/logstash
